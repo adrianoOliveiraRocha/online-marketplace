76821f515054172b9b92b43f784b147f2d8ac04c
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2019-03-08 15:02:50 -0300

    #

--- app/control/post.js
+++ app/control/post.js
@@ -5,38 +5,39 @@ module.exports.new_post = (req, res, application) => {
     });
   } else {
     var data = req.body;
+    res.send(data);
+    // var imageName = null;
+    // if (Object.keys(req.files).length > 0) {// image sended
+    //   let prefix = new Date().getTime() + '_';
+    //   console.log(`prefix: ${prefix}`);
+    //   imageName = prefix + req.files.image.name;
+    //   console.log(`imageName: ${imageName}`);
+    //   let image = req.files.image;
+    //   image.mv(__dirname + '/../public/upload/' + imageName, (err) => {
+    //     if (err) {
+    //       return res.status(500).send(err);
+    //     }
+    //   });
+    // }
 
-    var imageName = null;
-    if (Object.keys(req.files).length > 0) {// image sended
-      let prefix = new Date().getTime() + '_';
-      console.log(`prefix: ${prefix}`);
-      imageName = prefix + req.files.image.name;
-      console.log(`imageName: ${imageName}`);
-      let image = req.files.image;
-      image.mv(__dirname + '/../public/upload/' + imageName, (err) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-      });
-    }
+    // const Post = application.app.models.Post;
+    // var post = new Post(data.title, data.text, imageName);
+    // post.save(application, (err, result) => {
+    //   if(err) {
+    //     res.send(err.sqlMessage);
+    //   } else {
+    //     console.log(`Saved with id ${result['insertId']}`);
+    //     req.session.message = 'Novo post salvo com sucesso!';
+    //     res.redirect('\admin');
+    //   }
+    // });
 
-    const Post = application.app.models.Post;
-    var post = new Post(data.title, data.text, imageName);
-    post.save(application, (err, result) => {
-      if(err) {
-        res.send(err.sqlMessage);
-      } else {
-        console.log(`Saved with id ${result['insertId']}`);
-        req.session.message = 'Novo post salvo com sucesso!';
-        res.redirect('\admin');
-      }
-    });
   }
 }
 
 module.exports.show_posts = (req, res, application) => {
-  const Post = application.app.models.Post;
-  Post.getAll(application, (err, result) => {
+
+  application.app.models.Post.getAll(application, (err, result) => {
     if(err) {
       console.error(`Error: ${err}`);
     } else {
@@ -45,18 +46,19 @@ module.exports.show_posts = (req, res, application) => {
         'posts': result
       });
     }
-  })
-  
+  });  
 }
 
 module.exports.details_post = (req, res, application) => {
   var idPost = req.query.id;
-  const Post = application.app.models.Post;
-  var post = Post.get(idPost, application, (err, result) => {
+  application.app.models.Post.get(idPost, application, (err, result) => {
     if (err) {
       res.send(err.sqlMessage);
     } else {
-      res.send(result[0]);
+      res.render('admin/post/detail.ejs', {
+        'user': req.session.user,
+        'post': result[0]
+      });
     }
   });
 }
